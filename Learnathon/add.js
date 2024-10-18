cdbc25750c1dce9c71d0cc0876d2c3caa86bddfc
KATURINEERAJADEVI <142906804+KATURINEERAJADEVI@users.noreply.github.com>
2024-10-18 12:54:50 +0530

    2300032091_K NEERAJA DEVI

---
+++ Learnathon/add.js
@@ -0,0 +1,8 @@
+//var
+//let
+let a="zeroone";
+a=10;
+const n=10;
+//n="dinesh";
+
+console.log(n);
---
+++ Learnathon/info.js
@@ -0,0 +1,6 @@
+let a="K.NEERAJA DEVI";
+let b=2300032091;
+const c="CSE";
+console.log(a+"\n"+b+"\n"+c+"\n");
+
+
